e3d2a1547979fbdfd1a1f4a3515b286a6d3a9bd7
ojscom <ojscom@hanmail.net>
2018-07-26 14:51:47 +0900

    모듈 사용 import

---
+++ call_calc.py
@@ -0,0 +1,13 @@
+import calculator
+
+num1=calculator.add(10,2)
+print(num1)
+
+num2=calculator.sub(10,2)
+print(num2)
+
+num3=calculator.mul(10,2)
+print(num3)
+
+num4=calculator.div(10,2)
+print(num4)
