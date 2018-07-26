b4a815b88523dac739a0747ec30160c9b51310a0
ojscom <ojscom@hanmail.net>
2018-07-26 16:13:05 +0900

    로또, 계산기 import  프로그램

---
+++ call_cal2.py
@@ -0,0 +1,18 @@
+# from calculator import add #calcultor.py 에서 add 함수만 불러쓰고 싶을 때
+# result=add(1,2)
+# print(result)
+
+# from calculator import add,sub #calcultor.py 에서 add,sub 함수만 불러쓰고 싶을 때
+# result=add(1,2)
+# result2=sub(3,2)
+# print(result, result2)
+
+from calculator import *
+result=add(10,2)
+print(result)
+result=sub(10,2)
+print(result)
+result=mul(10,2)
+print(result)
+result=div(10,2)
+print(result)
