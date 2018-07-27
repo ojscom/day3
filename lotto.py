48178e90da94ca8e4de68d783ea4bdea95a64405
ojscom <ojscom@hanmail.net>
2018-07-27 09:16:31 +0900

    lotto 프로그램 변경

--- lotto.py
+++ lotto.py
@@ -1,29 +1,29 @@
 import random
 
 #로또 번호는 1~46, 총 6개 당첨번호
-today=[]
+today_lotto_num=[]
 
-# today.append(random.randrange(1,47,1)) 
-# today.append(random.randrange(1,47,1)) 
-# today.append(random.randrange(1,47,1)) 
-# today.append(random.randrange(1,47,1)) 
-# today.append(random.randrange(1,47,1)) 
-# today.append(random.randrange(1,47,1)) 
+# today_lotto_num.append(random.randrange(1,47,1)) 
+# today_lotto_num.append(random.randrange(1,47,1)) 
+# today_lotto_num.append(random.randrange(1,47,1)) 
+# today_lotto_num.append(random.randrange(1,47,1)) 
+# today_lotto_num.append(random.randrange(1,47,1)) 
+# today_lotto_num.append(random.randrange(1,47,1)) 
 
 # start=0
 # while start<6:
-#     today.append(random.randrange(1,47,1))
+#     today_lotto_num.append(random.randrange(1,47,1))
 #     start+=1
 
 # for i in range(0,6):
-#     today.append(random.randrange(1,47,1))    
+#     today_lotto_num.append(random.randrange(1,47,1))    
 
-today=random.sample(range(1,47),6)    
+today_lotto_num=random.sample(range(1,47),6)    
  
 print("오늘의 추천번호는 "
-+str(today[0])+","
-+str(today[1])+","
-+str(today[2])+","
-+str(today[3])+","
-+str(today[4])+","
-+str(today[5])+" 입니다." )
++str(today_lotto_num[0])+","
++str(today_lotto_num[1])+","
++str(today_lotto_num[2])+","
++str(today_lotto_num[3])+","
++str(today_lotto_num[4])+","
++str(today_lotto_num[5])+" 입니다." )
